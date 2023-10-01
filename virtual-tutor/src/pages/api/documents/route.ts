5998ade7936f47b90a5fa7c6d8c40df1aba1725c
Y <1yl3@queensu.ca>
2023-10-01 18:55:34 -0400

    Upload working

--- virtual-tutor/src/pages/api/documents/route.ts
+++ virtual-tutor/src/pages/api/documents/route.ts
@@ -1,4 +1,5 @@
 // app/api/documents/route.ts
+import formidable from 'formidable-serverless';
 import { NextRequest, NextResponse } from "next/server";
 
 import {
@@ -16,24 +17,55 @@ const s3 = new S3Client({
   },
 });
 
-// endpoint to get the list of files in the bucket
-export async function GET() {
-  const response = await s3.send(new ListObjectsCommand({ Bucket }));
-  return NextResponse.json(response?.Contents ?? []);
+export default async function handler(req, res) {
+  //res.status(200).json({ message: 'Hello from the API route!' });
+    const form = new formidable.IncomingForm();
+
+  // Parse the incoming form data
+  const parseResult = await new Promise((resolve, reject) => {
+    form.parse(req, (err, fields, files) => {
+      if (err) {
+        reject(err);
+      } else {
+        resolve({ fields, files });
+      }
+    });
+  });
+
+  const { files } = parseResult;
+
+  try {
+     res = await Promise.all(
+      Object.values(files).map(async (file: formidable.File) => {
+        // Read the file into a Buffer
+        const Body = await new Promise<Buffer>((resolve, reject) => {
+          const readStream = require('fs').createReadStream(file.path);
+
+          const chunks: Buffer[] = [];
+          readStream.on('data', (chunk: Buffer) => {
+            chunks.push(chunk);
+          });
+
+          readStream.on('end', () => {
+            const fileBuffer = Buffer.concat(chunks);
+            resolve(fileBuffer);
+          });
+
+          readStream.on('error', (error: any) => {
+            reject(error);
+          });
+        });
+
+        // Upload the file to S3
+        await s3.send(new PutObjectCommand({ Bucket, Key: file.name, Body }));
+
+        return { fileName: file.name, uploaded: true };
+      })
+    );
+
+    return NextResponse.json(res);
+  } catch (error) {
+    console.error('Error uploading files to S3:', error);
+    return NextResponse.error('Failed to upload files', 500);
 }
-
-// endpoint to upload a file to the bucket
-export default async function POST(request: NextRequest) {
-  const formData = await request.formData();
-  const files = formData.getAll("file") as File[];
-
-  const response = await Promise.all(
-    files.map(async (file) => {
-      // not sure why I have to override the types here
-      const Body = (await file.arrayBuffer()) as Buffer;
-      s3.send(new PutObjectCommand({ Bucket, Key: file.name, Body }));
-    })
-  );
-
-  return NextResponse.json(response);
 }
--- virtual-tutor/src/pages/index.tsx
+++ virtual-tutor/src/pages/index.tsx
@@ -1,18 +1,7 @@
-import useSWR from "swr";
 import { Image } from "@mantine/core";
 //import ImagePicker from "../components/ImagePicker";
 
 const fetcher = (path: string) => fetch(path).then((res) => res.json());
-
-const Images = () => {
-  const { data } = useSWR<{ Key?: string }[]>("/api/documents", fetcher)
-  return data?.map((image) => <S3Image Key={image.Key} />)
-}
-
-const S3Image = ({ Key }: { Key: string }) => {
-  const { data } = useSWR<{ src: string }>(`/api/documents/${Key}`, fetcher)
-  return <Image src={data.src} />
-}
 
 export default function Home() {
   return (
