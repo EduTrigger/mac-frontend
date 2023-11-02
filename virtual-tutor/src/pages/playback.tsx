afd261bf2701ef8b4db2dea1115b267e9f4b53bd
xyanglu <1yl3@queens.ca>
2023-11-02 13:29:23 -0400

    Playback working

--- virtual-tutor/src/pages/playback.tsx
+++ virtual-tutor/src/pages/playback.tsx
@@ -1,56 +1,73 @@
 import { useEffect, useState } from "react";
 import { createClient } from "@supabase/supabase-js";
-import { AspectRatio, Button, Table, Text } from "@mantine/core";
+import { AspectRatio, Table, Text } from "@mantine/core";
 
-const supabaseUrl = "YOUR_SUPABASE_URL";
-const supabaseKey = "YOUR_SUPABASE_KEY";
-const supabase = createClient(supabaseUrl, supabaseKey);
+const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL;
+const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY;
+
+const supabase = createClient(supabaseUrl, supabaseAnonKey);
 
 export default function Demo() {
-  const [videos, setVideos] = useState([]);
-  const [selectedVideo, setSelectedVideo] = useState(null);
+  const defaultVideoUrl = "https://www.youtube.com/embed/dQw4w9WgXcQ?si=Fvl0ZUvH-6flAbdh";
+
+  const [videos, setVideos] = useState([
+    {
+      title: "Default Video",
+      video_url: defaultVideoUrl,
+      status: "clip_ready",
+      id: 0, // Replace with the actual ID you want to use
+    },
+  ]);
+
+  const [selectedVideo, setSelectedVideo] = useState(videos[0]);
 
   useEffect(() => {
     // Fetch data from Supabase table
     async function fetchVideos() {
-      const { data, error } = await supabase.from("videos").select("*");
+      const { data, error } = await supabase
+        .from("S3_URL")
+        .select();
       if (error) {
         console.error("Error fetching videos:", error);
       } else {
         setVideos(data);
+        console.log("Fetched data:", data); // Log the data
       }
     }
 
     fetchVideos();
   }, []);
 
-  const handleVideoClick = (video) => {
+  const handleRowClick = (video) => {
     setSelectedVideo(video);
   };
 
   return (
-    <div>
-      <div style={{ display: "flex", gap: "20px" }}>
-        <Table
-          data={videos}
-          onRowClick={handleVideoClick}
-          rowKey="id"
-          columns={[
-            { name: "title", title: "Title" },
-            { name: "url", title: "URL" },
-          ]}
-        />
-        {selectedVideo && (
-          <AspectRatio ratio={16 / 9}>
-            <iframe
-              src={selectedVideo.url}
-              title="YouTube video player"
-              frameBorder="0"
-              allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-            />
-          </AspectRatio>
-        )}
-      </div>
-    </div>
+<div className="flex">
+  <div className="w-1/2">
+    <AspectRatio ratio={1920 / 1080}>
+      <iframe
+        src={selectedVideo.video_url}
+        title="Video player"
+        frameBorder="0"
+        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+		allowFullScreen
+      />
+    </AspectRatio>
+  </div>
+  <div className="w-1/2">
+	<Table className="custom-table">
+      <Table.Tbody>
+        {videos.map((video) => (
+            <Table.Tr onClick={() => handleRowClick(video)}>
+              <Table.Td>{video.video_url}</Table.Td>
+			                <Table.Td>{video.status}</Table.Td>
+            </Table.Tr>
+        ))}
+      </Table.Tbody>
+    </Table>
+  </div>
+</div>
+
   );
 }
--- virtual-tutor/src/pages/upload.tsx
+++ virtual-tutor/src/pages/upload.tsx
@@ -1,16 +1,7 @@
 import { Group, Text, rem, Progress } from '@mantine/core';
 import { Dropzone, DropzoneProps, MIME_TYPES } from '@mantine/dropzone';
 import { useS3Upload } from "next-s3-upload";
-
-function getIconColor(status, theme) {
-  return status.accepted
-    ? theme.colors[theme.primaryColor][6]
-    : status.rejected
-    ? theme.colors.red[6]
-    : theme.colorScheme === 'dark'
-    ? theme.colors.dark[0]
-    : theme.black;
-}
+import { IconUpload, IconPhoto, IconX } from '@tabler/icons-react';
 
 export default function BaseDemo(props: Partial<DropzoneProps>) {
   const { uploadToS3, files } = useS3Upload();
@@ -33,17 +24,36 @@ export default function BaseDemo(props: Partial<DropzoneProps>) {
         accept={[MIME_TYPES.mp4]}
         {...props}
       >
-        <Group justify="center" gap="xl" mih={220} style={{ pointerEvents: 'none' }}>
-          <div>
-            <Text size="xl" inline>
-              Drag images here or click to select files
-            </Text>
-            <Text size="sm" c="dimmed" inline mt={7}>
-              Attach as many files as you like, each file should not exceed 1GB
-            </Text>
-          </div>
-        </Group>
-      </Dropzone>
+      <Group justify="center" gap="xl" mih={220} style={{ pointerEvents: 'none' }}>
+        <Dropzone.Accept>
+          <IconUpload
+            style={{ width: rem(52), height: rem(52), color: 'var(--mantine-color-blue-6)' }}
+            stroke={1.5}
+          />
+        </Dropzone.Accept>
+        <Dropzone.Reject>
+          <IconX
+            style={{ width: rem(52), height: rem(52), color: 'var(--mantine-color-red-6)' }}
+            stroke={1.5}
+          />
+        </Dropzone.Reject>
+        <Dropzone.Idle>
+          <IconPhoto
+            style={{ width: rem(52), height: rem(52), color: 'var(--mantine-color-dimmed)' }}
+            stroke={1.5}
+          />
+        </Dropzone.Idle>
+
+        <div>
+          <Text size="xl" inline>
+            Drag images here or click to select files
+          </Text>
+          <Text size="sm" c="dimmed" inline mt={7}>
+            Attach as many files as you like, each file should not exceed 5mb
+          </Text>
+        </div>
+      </Group>
+    </Dropzone>
 
 <div className="pt-8" style={{ color: 'white', display: 'flex', flexDirection: 'column' }}>
   {files.map((file, index) => (
