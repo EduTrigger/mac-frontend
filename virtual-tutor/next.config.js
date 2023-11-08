24029c08b37acbab0eaa5933b8ef25e6de2c84c8
Liqing Pan <dprat0821@gmail.com>
2023-11-08 14:43:56 -0500

    add a page for further testing

--- virtual-tutor/next.config.js
+++ virtual-tutor/next.config.js
@@ -7,7 +7,19 @@ module.exports = {
     'tailwindcss/nesting': {},
     tailwindcss: {},
     autoprefixer: {},
-  }
+  },
+  env: {
+    AWS_REGION: process.env.AWS_REGION,
+    AMPLIFY_BUCKET: process.env.AMPLIFY_BUCKET,
+    AWS_ACCESS_KEY_ID: process.env.AWS_ACCESS_KEY_ID,
+    AWS_SECRET_ACCESS_KEY: process.env.AWS_SECRET_ACCESS_KEY,
+    S3_UPLOAD_KEY: process.env.S3_UPLOAD_KEY,
+    S3_UPLOAD_SECRET: process.env.S3_UPLOAD_SECRET,
+    S3_UPLOAD_BUCKET: process.env.S3_UPLOAD_BUCKET,
+    S3_UPLOAD_REGION: process.env.S3_UPLOAD_REGION,
+    NEXT_PUBLIC_SUPABASE_URL: process.env.NEXT_PUBLIC_SUPABASE_URL,
+    NEXT_PUBLIC_SUPABASE_ANON_KEY: process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY,
+  },
 };
 
 console.log('Custom Next.js configuration loaded');
